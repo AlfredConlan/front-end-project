a7699cb6533d1050b62643f0d4037dd5f35c3629
AlfredConlan <oconellan@gmail.com>
2021-08-16 19:20:23 -0500

    added code to console log data from 3 apis and widgets

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
-function callPlayerAPI() {
+// Default call to covid19api.com which lists all the current routes available with details on each
+function callCovid19API() {
   let catObject = $.ajax({
-    url: "http://api.espn.com/v1/sports/football/nfl/athletes",
+    url: "https://api.covid19api.com/",
     contentType: "application/json",
     dataType: "json",
     success: function (result) {},
   }).done(function (obj) {
-    //catObject = obj;
     console.log(obj);
-    //showCats(catObject);
+    callMmediaGroup();
+  });
+}
+
+// Default call to mmediagroup.fr which lists current cases for different countries
+function callMmediaGroup() {
+  let catObject = $.ajax({
+    url: "https://covid-api.mmediagroup.fr/v1/cases",
+    contentType: "application/json",
+    dataType: "json",
+    success: function (result) {},
+  }).done(function (obj) {
+    console.log(obj);
+    callOxford();
+  });
+}
+
+// Default call to Oxford which lists daily info for a selected country
+function callOxford() {
+  let catObject = $.ajax({
+    url: "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/actions/usa/2021-08-5",
+    contentType: "application/json",
+    dataType: "json",
+    success: function (result) {},
+  }).done(function (obj) {
+    console.log(obj);
+  });
+}
+
+// Default call to Oxford which lists daily info for a selected country
+function callOxford() {
+  let catObject = $.ajax({
+    url: "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/actions/usa/2021-08-5",
+    contentType: "application/json",
+    dataType: "json",
+    success: function (result) {},
+  }).done(function (obj) {
+    console.log(obj);
   });
 }
